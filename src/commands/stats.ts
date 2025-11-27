f8ec109e1bf3bf30998762e5ea2c366fd8a2b2a1
etonstro <tonstrom.egert@gmail.com>
2025-11-27 16:36:12 +0200

    add stats and json to bot

---
+++ src/commands/stats.ts
@@ -0,0 +1,48 @@
+import { Command } from "@sapphire/framework";
+import { EmbedBuilder } from "discord.js";
+import { ChatInputCommandInteraction } from "discord.js";
+import { readFileSync, existsSync } from "fs";
+import path from "path";
+
+export class StatsCommand extends Command {
+  constructor(context: Command.LoaderContext, options: Command.Options) {
+    super(context, { ...options });
+  }
+
+  public override registerApplicationCommands(registry: Command.Registry) {
+    registry.registerChatInputCommand((builder) =>
+      builder
+        .setName("stats")
+        .setDescription("Vaata serveri top 10 jutustajaid")
+    );
+  }
+
+  public override async chatInputRun(interaction: ChatInputCommandInteraction) {
+    await interaction.deferReply();
+
+    const filePath = path.join(process.cwd(), "stats.json");
+
+    if (!existsSync(filePath)) {
+      await interaction.editReply("Messages.json faili ei leitud!");
+      return;
+    }
+
+    const content = readFileSync(filePath, "utf8");
+    const data: Record<
+      string,
+      { displayName: string; messagesCount: number; words: number }
+    > = JSON.parse(content);
+
+    const leaderboard = Object.values(data).sort((a, b) => b.words - a.words);
+    const topList = leaderboard
+      .map((u, i) => `${i + 1}. **${u.displayName}** - ${u.words} sõna`)
+      .slice(0, 10)
+      .join("\n");
+    const embed = new EmbedBuilder()
+      .setTitle("Top 10")
+      .setDescription(topList)
+      .setColor("#db759c");
+
+    await interaction.editReply({ embeds: [embed] });
+  }
+}
---
+++ src/listeners/messageCreateListener.ts
@@ -0,0 +1,51 @@
+import { Listener } from "@sapphire/framework";
+import type { Message } from "discord.js";
+import { writeFileSync, existsSync, readFileSync } from "fs";
+import path from "path";
+
+export class MessageCreateListener extends Listener {
+  public constructor(context: Listener.Context) {
+    super(context, {
+      event: "messageCreate",
+    });
+  }
+
+  public run(message: Message) {
+    if (message.author.bot) return;
+
+    const filePath = path.join(process.cwd(), "stats.json");
+
+    let data: Record<
+      string,
+      { displayName: string; messagesCount: number; words: number }
+    > = {};
+    if (existsSync(filePath)) {
+      const content = readFileSync(filePath, "utf8");
+      data = JSON.parse(content);
+    }
+
+    const authorId = message.author.id;
+    const wordsCount = message.content
+      .trim()
+      .split(/\s+/) // jagab sõnad tühikute, tabide, reavahetuste järgi
+      .filter(Boolean).length; // eemaldab tühjad kohad
+
+    if (!data[authorId]) {
+      data[authorId] = {
+        displayName: message.member?.displayName || message.author.username,
+        messagesCount: 1,
+        words: wordsCount,
+      };
+    } else {
+      data[authorId].messagesCount++;
+      data[authorId].displayName =
+        message.member?.displayName || message.author.username;
+      data[authorId].words += wordsCount;
+    }
+
+    writeFileSync(filePath, JSON.stringify(data, null, 2));
+    console.log(
+      `Salvestatud sõnum: ${data[authorId].displayName} - ${data[authorId].messagesCount} sõnumit kokku`
+    );
+  }
+}
